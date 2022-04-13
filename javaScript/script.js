a399da373d6aae6baedc3b14e5971d3d9cd92313
Abhijeet Singla <95906204+AbhijeetSingla@users.noreply.github.com>
2022-04-14 02:59:54 +0530

    feat: added theme toggle button

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,17 +1,18 @@
 let dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 let light = window.matchMedia("(prefers-color-scheme: light)").matches;
-const toggle = document.querySelector('link[id="mode-select"]')
-console.log("matched");
+const themeCSSPath = document.querySelector('link[id="mode-select"]');
+const themeToggleButton = document.querySelector("#theme-toggle-button");
 
-let html = document.querySelector("html");
+function toggleTheme() {
+    themeCSSPath.setAttribute("href", "/css/light.css");
+}
 
 if (light) {
-	html.setAttribute("class", "light-mode");
-    toggle.setAttribute("href", "/css/light.css")
+	themeCSSPath.setAttribute("href", "/css/light.css");
 } else if (dark) {
-	html.setAttribute("class", "dark-mode");
-    toggle.setAttribute("href", "/css/dark.css")
+	themeCSSPath.setAttribute("href", "/css/dark.css");
 } else {
-    html.setAttribute("class", "light-mode");
-    toggle.setAttribute("href", "/css/light.css")
+    themeCSSPath.setAttribute("href", "/css/light.css");
 }
+
+themeToggleButton.addEventListener("click", toggleTheme);
