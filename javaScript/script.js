54fd2f18ce9d0b2e0288942c874610e49edc070a
Abhijeet Singla <95906204+AbhijeetSingla@users.noreply.github.com>
2022-04-14 13:26:42 +0530

    fix: local storage

--- javaScript/script.js
+++ javaScript/script.js
@@ -6,24 +6,30 @@ const themeToggleButton = document.querySelector("#theme-toggle-button");
 
 function toggleTheme() {
     darkMode = localStorage.getItem("darkMode");
+    console.log(darkMode + "4");
     if (darkMode === "enabled") {
-        darkMode = localStorage.setItem("darkMode", "disabled");
+        localStorage.setItem("darkMode", "disabled");
         themeCSSPath.setAttribute("href", "/css/light.css");
+        console.log(darkMode + "5");
     } else {
-        darkMode = localStorage.setItem("darkMode", "enabled");
+        localStorage.setItem("darkMode", "enabled");
         themeCSSPath.setAttribute("href", "/css/dark.css");
+        console.log(darkMode + "6");
     }
 }
 
 if (light) {
 	themeCSSPath.setAttribute("href", "/css/light.css");
-    darkMode = localStorage.setItem("darkMode", "disabled");
+    localStorage.setItem("darkMode", "disabled");
+    console.log(darkMode + "1");
 } else if (dark) {
 	themeCSSPath.setAttribute("href", "/css/dark.css");
-    darkMode = localStorage.setItem("darkMode", "enabled");
+    localStorage.setItem("darkMode", "enabled");
+    console.log(darkMode + "2");
 } else {
     themeCSSPath.setAttribute("href", "/css/light.css");
-    darkMode = localStorage.setItem("darkMode", "disabled");
+    localStorage.setItem("darkMode", "disabled");
+    console.log(darkMode + "3");
 }
 
 themeToggleButton.addEventListener("click", toggleTheme);
