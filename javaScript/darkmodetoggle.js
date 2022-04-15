69812cc2ad73778b954fcfdef2b8a1907c556cd5
Abhijeet Singla <95906204+AbhijeetSingla@users.noreply.github.com>
2022-04-16 01:08:06 +0530

    feat: added support for importing script
    due to use of paths in the script, it needed to be adjusted accordingly so that it could be directly imported into webpages.

--- javaScript/darkmodetoggle.js
+++ javaScript/darkmodetoggle.js
@@ -3,37 +3,40 @@ let dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 let light = window.matchMedia("(prefers-color-scheme: light)").matches;
 const themeCSSPath = document.querySelector('link[id="mode-select"]');
 const themeToggleButton = document.querySelector("#theme-toggle-button");
+const initialPath = themeCSSPath.getAttribute("href");
+const lightLink = initialPath + "/css/light.css";
+const darkLink = initialPath + "/css/dark.css";
 
 function toggleTheme() {
     darkMode = localStorage.getItem("darkMode");
 
     if (darkMode === "enabled") {
         localStorage.setItem("darkMode", "disabled");
-        themeCSSPath.setAttribute("href", "/css/light.css");    
+        themeCSSPath.setAttribute("href", lightLink);    
     } else if (dark && darkMode === null) {
         localStorage.setItem("darkMode", "disabled");
-        themeCSSPath.setAttribute("href", "/css/light.css");
+        themeCSSPath.setAttribute("href", lightLink);
     } else if (light && darkMode === null) {
         localStorage.setItem("darkMode", "enabled");
-        themeCSSPath.setAttribute("href", "/css/dark.css");
+        themeCSSPath.setAttribute("href", darkLink);
     } else {
         localStorage.setItem("darkMode", "enabled");
-        themeCSSPath.setAttribute("href", "/css/dark.css");    
+        themeCSSPath.setAttribute("href", darkLink);    
     }
 }
 
 if (light && darkMode === null) {
-	themeCSSPath.setAttribute("href", "/css/light.css");
+	themeCSSPath.setAttribute("href", lightLink);
 } else if (dark && darkMode === null) {
-	themeCSSPath.setAttribute("href", "/css/dark.css");
+	themeCSSPath.setAttribute("href", darkLink);
 } else {
-    themeCSSPath.setAttribute("href", "/css/light.css");
+    themeCSSPath.setAttribute("href", lightLink);
 }
 
 if (darkMode === "disabled") {
-    themeCSSPath.setAttribute("href", "/css/light.css");
+    themeCSSPath.setAttribute("href", lightLink);
 } else if (darkMode === "enabled") {
-    themeCSSPath.setAttribute("href", "/css/dark.css");
+    themeCSSPath.setAttribute("href", darkLink);
 } else {}
 
 themeToggleButton.addEventListener("click", toggleTheme);
